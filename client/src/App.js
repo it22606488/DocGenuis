4787a99bfc72b29cd2bb3082d485634d9ab71445
dinith-rusiru <it22145284@my.sliit.lk>
2025-05-15 00:17:32 +0530

    Revert "."
    This reverts commit 9eeaf5acfb2589c6242d47408623d434c0f6a18f.

--- client/src/App.js
+++ client/src/App.js
@@ -7,6 +7,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Login from './components/Login';
 import Register from './components/Register';
 import FileUpload from './components/FileUpload';
+import DocumentSearch from './components/DocumentSearch';
 import Dashboard from './components/Dashboard';
 import Navbar from './components/Navbar';
 import DocumentView from './components/DocumentView';
@@ -86,7 +87,7 @@ function App() {
 
           <Route path="/search" element={
             <ProtectedRoute>
-    
+              <DocumentSearch />
               <Search />
 
             </ProtectedRoute>
